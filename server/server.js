95f14899fb56b3715e1b138450e2b0054eef4459
sang nguyen <vinhsang97@gmail.com>
2023-02-16 21:37:59 +0700

    update

--- server/server.js
+++ server/server.js
@@ -31,12 +31,11 @@ app.post('/', async (req, res) => {
       model: "text-davinci-003",
       // model: "code-davinci-002",
       prompt: `${prompt}`,
-      temperature: 0.25, // Higher values means the model will take more risks.
+      temperature: 0, // Higher values means the model will take more risks.
       max_tokens: 3500, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 0.1, // alternative to sampling with temperature, called nucleus sampling
-      logprobs: 5,
-      frequency_penalty: 0.4, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0.8, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
 
